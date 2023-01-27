96f2030616774a34c264c4c8be526d9d271abd8b
akramuzzaman <siddique.riseuplabs@gmail.com>
2023-01-27 15:40:16 +0600

    my project

---
+++ app/src/main/java/com/example/my_family/HomeScreen.kt
@@ -0,0 +1,18 @@
+package com.example.my_family
+
+import android.annotation.SuppressLint
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.google.android.material.bottomnavigation.BottomNavigationView
+
+class HomeScreen : AppCompatActivity() {
+    @SuppressLint("MissingInflatedId")
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_home_screen)
+
+        var bottomBar = findViewById<BottomNavigationView>(R.id.bottom_bar)
+
+    }
+
+}
